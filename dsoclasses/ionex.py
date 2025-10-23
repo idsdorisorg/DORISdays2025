39e34e278d5cf33bd262507283593bd37848ece3
xanthos@durruti <xanthos.papanikolaou@gmail.com>
2025-10-23 14:52:04 +0300

    moving

--- dsoclasses/ionex.py
+++
@@ -1,8 +0,0 @@
-
-class Ionex:
-    def read_header(self, fn):
-        with open(fn, 'r') as fin:
-            line = fin.readline()
-            self.version = float(line[0:8])
-            assert line[20] == 'I'
-            self.satsys = line[40:60].strip()
